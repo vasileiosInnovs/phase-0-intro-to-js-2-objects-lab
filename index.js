e39a958058ccdb769b57bad160c0befa3ce90abd
hagee <hagee.ktr@outlook.com>
2025-03-01 11:30:42 +0300

    Modify objects using functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name : " ",
+    streetAddress : " "
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const newEmployee = {...employee};
+    newEmployee[name] = streetAddress;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    employee[name] = streetAddress;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    let newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
